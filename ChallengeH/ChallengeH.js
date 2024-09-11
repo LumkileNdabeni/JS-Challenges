79424e2dccbc567cdc3a2a8e8dd0f69b187dd787
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-09-11 15:02:40 +0200

    Completed ChallengeH

---
+++ ChallengeH/ChallengeH.js
@@ -0,0 +1,11 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = !isNaN(parseInt(primaryPhone)) && typeof primaryPhone === 'string';
+const secondaryValid = !isNaN(parseInt(secondaryPhone)) && typeof secondaryPhone === 'string';
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
+ 
