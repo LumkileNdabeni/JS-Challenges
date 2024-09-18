2370cd94938864cd9cd917804df02a8c0fbcda05
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-09-18 11:20:12 +0200

    Added commenting

--- ChallengeH/ChallengeH.js
+++ ChallengeH/ChallengeH.js
@@ -3,9 +3,13 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
+// Check if primaryPhone is a valid numerical string
 const primaryValid = !isNaN(parseInt(primaryPhone)) && typeof primaryPhone === 'string';
+
+// Check if secondaryPhone is a valid numerical string
 const secondaryValid = !isNaN(parseInt(secondaryPhone)) && typeof secondaryPhone === 'string';
 
+// Log the validation results to the console
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
  
